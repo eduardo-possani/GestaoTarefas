54173d2d2bb193ceda14289a40b3d6e4287bc2bd
eduardo-possani <possaniduardo@gmail.com>
2023-09-07 14:35:25 -0300

    fix: router atualização

--- lumen-back/app/Http/Controllers/SubtarefaController.php
+++ lumen-back/app/Http/Controllers/SubtarefaController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use App\Models\Subtarefa; 
 
-class SubtarefasController extends Controller
+class SubtarefaController extends Controller
 {
     public function busca(){
         $subtarefas = Subtarefa::all();
--- lumen-back/routes/web.php
+++ lumen-back/routes/web.php
@@ -14,14 +14,14 @@ use App\Http\Controllers\SubtarefasController;
 
 $router->group(['prefix' => 'api'], function () use ($router) {
     // Rotas para TarefaController
-    $router->get('/tarefas', 'TarefaController@busca');
-    $router->post('/tarefas', 'TarefaController@create');
-    $router->put('/tarefas/{id}', 'TarefaController@update');
-    $router->delete('/tarefas/{id}', 'TarefaController@apreensao');
+    $router->get('/busca', 'TarefaController@busca');
+    $router->post('/ciar', 'TarefaController@create');
+    $router->put('/atualizar/{id}', 'TarefaController@update');
+    $router->delete('/deletar/{id}', 'TarefaController@apreensao');
 
     // Rotas para SubtarefasController
-    $router->get('/subtarefas', 'SubtarefasController@busca');
-    $router->post('/subtarefas', 'SubtarefasController@create');
-    $router->put('/subtarefas/{id}', 'SubtarefasController@update');
-    $router->delete('/subtarefas/{id}', 'SubtarefasController@apreensao');
+    $router->get('/buscasub', 'SubtarefasController@busca');
+    $router->post('/criasub', 'SubtarefasController@create');
+    $router->put('/atualizasub/{id}', 'SubtarefasController@update');
+    $router->delete('/deletasub/{id}', 'SubtarefasController@apreensao');
 });
